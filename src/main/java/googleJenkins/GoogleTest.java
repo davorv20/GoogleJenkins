6acd10a701722d0ebc7df739fbe4f7550e89e782
dvajda <davor.vajda@og-cs.hr>
2019-11-21 13:21:25 +0100

    update

--- src/main/java/googleJenkins/GoogleTest.java
+++ src/main/java/googleJenkins/GoogleTest.java
@@ -11,18 +11,18 @@ import org.testng.annotations.Test;
 public class GoogleTest {
 
 	public static WebDriver driver;
-		
+
 	@BeforeMethod
 	public void beforeMethod() {
 		System.out.print("Before method start \n");
-        System.setProperty("driver.wedriver.chrome", "chromedriver.exe");
-        driver = new ChromeDriver();
+		System.setProperty("driver.wedriver.chrome", "chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		System.out.print("Before method finish \n");
 
 	}
-	
+
 	@AfterMethod
 	public void afterMethod() {
 		driver.close();
