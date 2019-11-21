a2fb4bb646769aad8f6bde94244671f0d4ce0564
dvajda <davor.vajda@og-cs.hr>
2019-11-21 10:01:37 +0100

    update

--- src/main/java/hr/ogcs/qa/GoogleJenkins/App.java
+++ src/main/java/hr/ogcs/qa/GoogleJenkins/App.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+
 import org.testng.annotations.Test;
 /**
  * Hello world!
@@ -16,6 +18,7 @@ public class App extends GoogleTest
 	@Test
 	@Step("Enter into web page")
 	@Description("Test Description: Enter into google.hr ")
+	@Story("Story 1")
 	public void openGoogle() throws InterruptedException {
 		System.out.println("Test start with entering web address");
 		driver.get("https://www.google.hr/");
@@ -25,6 +28,7 @@ public class App extends GoogleTest
 
 	@Step("Search content on google")
 	@Description("Search some stuff on net")
+	@Story("Story 2")
 	public void searchStiff() throws InterruptedException {
 		System.out.println("Enter and search");
 		WebElement element = driver.findElement(By.className("gLFyf gsfi"));
